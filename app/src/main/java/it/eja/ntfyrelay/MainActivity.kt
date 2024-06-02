319338bb1803916e39f1ccf3d23a7879c9ab06a5
Ubaldo Porcheddu <ubaldo@eja.it>
2024-06-02 17:14:13 +0100

    normalize

--- app/src/main/java/it/eja/ntfyrelay/MainActivity.kt
+++ app/src/main/java/it/eja/ntfyrelay/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout)
+        setContentView(R.layout.main_activity)
 
         val enabled = isNotificationListenerEnabled(this)
         if (!enabled) {
